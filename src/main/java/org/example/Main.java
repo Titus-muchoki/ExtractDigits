625f945f323c606a39182211554e143fda4ad892
kajela <titoyut56@gmail.com>
2022-07-16 10:19:50 -0400

    add the initial files

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+//package org.example;
+//
+//public class Main {
+//
+//        public static void main(String[] args) {
+//            Scanner sc = new Scanner(System.in);
+//            System.out.print("Enter number to be extracted: ");
+//            int n = sc.nextInt();
+//            for (int i = 0; i < 5; i++) {
+//                System.out.println(n % 10);
+//            }
+//            n /= 10;
+//        }
+//        System.out.println();
+//    int number2 = 12345;
+//    String s = "" + number2;
+//    for(int i = 0; i<s.length(); i++)
+//
+//    {
+//        int t = Integer.parseInt(s.charAt(i) + "");
+//        System.out.println(t);
+//    }
+//    }
+//}
